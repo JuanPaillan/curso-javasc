37aa96bd1af58f8c66dbc6351be61fc922b402b7
Juan Paillan <juanedgardo.paillan@alumnos.ulagos.cl>
2024-08-28 23:03:55 -0400

    Agregue el holamundo

---
+++ variables.js
@@ -0,0 +1,24 @@
+
+var estatura = 1.71
+let peso = 60
+
+const nombre ="Juan"
+
+//Declarando las variables
+var edad
+let ciudad
+
+//Inicializando las variables
+edad = 22
+ciudad = "Quinched"
+
+const apellido = "Paillan"
+
+//Concatenando variables de tipo string
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
+
+//uso incorrecto de declaracion de variables
+let docente = 'Tomas', age = 25, asignaturas = ['IA', 'Seguridad Informatica'];
+
+
